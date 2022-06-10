3fefaa0c23573e88e27bc2f50394a0459973548b
Vladislav Surin <vlad_surin@mail.ru>
2022-06-11 01:25:20 +0300

    finished homework-8

--- homework-8/js/basket.js
+++ homework-8/js/basket.js
@@ -1,5 +1,19 @@
 'use strict';
 
+/*
+  Спасибо вам огромное за курс, вы отлично преподаете. Все было очень понятно и
+доступно. Я очень много новых знаний получил и понял, то что раньше казалось
+какой то магией. До этого учился в другом месте, не буду называть. Но после
+того обучения на котором изучению js отвелось 2 недели, 8 уроков по часу,
+я толком не понимал, как написать цикл для того что бы вывести консоль числа
+Фибонначчи, про что то сложнее даже вспоминать страшно. После окончания
+за код 2 месяца не садился, реально какой то страх был, что опять ничего не
+пойму. Но после вашего курса понял, что дело в преподавателе и подаче материала.
+Конечно не все дается легко, но уже сейчас могу решать какие то задачи.
+А уж месяц не мог и подумать, что смогу логику для корзины написать. Еще раз
+выражаю благодарнось за то, что смогли донести эти знания)
+*/
+
 const iconBasket = document.querySelector('.cartIcon');
 const basketCount = document.querySelector('.basketCount');
 const basket = document.querySelector('.basket');
@@ -30,6 +44,7 @@ function addToCart(id, name, price) {
     basketObj[id].count++;
     basketCount.textContent = sumBasketCount();
     basketTotalValue.textContent = sumBasketPrice();
+    addProductInBasket(id);
 }
 
 function sumBasketCount() {
@@ -46,4 +61,35 @@ function sumBasketPrice() {
         sum += product.price * product.count;
     }
    return sum;
+}
+
+function productInBasket(id) {
+    const product = `
+    <div class="basketRow" data-productId="${id}">
+        <div>${basketObj[id].name}</div>
+        <div>
+            <span class="productCount">${basketObj[id].count}</span> шт.
+        </div>
+        <div>$${basketObj[id].price}</div>
+        <div>
+            $<span class="productTotalCount">${basketObj[id].price *
+            basketObj[id].count}</span>
+        </div>
+    </div>
+    `;
+    basketTotal.insertAdjacentHTML('beforebegin', product);
+}
+
+function addProductInBasket(id) {
+    const basketProduct = document
+        .querySelector(`.basketRow[data-productId="${id}"]`);
+    if (!basketProduct) {
+        productInBasket(id);
+        return;
+    }
+
+    basketProduct.querySelector('.productCount')
+        .textContent = basketObj[id].count;
+    basketProduct.querySelector('.productTotalCount')
+        .textContent = basketObj[id].price * basketObj[id].count;
 }
